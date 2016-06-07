7b5b52ef7ee5d2e0bc7b9a881685b7c26d90916b
Camila Ramos <camirgarzon@gmail.com>
2016-06-07 00:32:02 -0700

    passed first test

--- exercises/rawstrings.js
+++ exercises/rawstrings.js
@@ -11,9 +11,9 @@ Feel free to use any string methods you wish.
 
 // Gives back ("returns") the fourth letter in a string
 var printLetter1 = function () { 
-  'print the fourth letter in this string'[3];
+  var string = 'print the fourth letter in this string'[3];
 
-  return
+  return string;
 };
 
 /*
